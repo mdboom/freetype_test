d6dfa046bfa80bb6c040872406c53651eef1584b
Michael Droettboom <mdboom@gmail.com>
2012-03-21 10:54:39 -0400

    Test different kinds of hinting

--- freetype_test.c
+++ freetype_test.c
@@ -7,15 +7,40 @@
 #define VERTICAL_HINTING
 #define HORIZ_HINTING 8
 
-int main(int argc, char** argv)
+int
+display_cbox(const char* name, unsigned long flags,
+             unsigned long charcode, FT_Face face) {
+  int error;
+  FT_Glyph glyph;
+  FT_BBox bbox;
+
+  if (error = FT_Load_Char(face, charcode, flags)) {
+    printf("FT_Load_Char error: 0x%x\n", error);
+    return 1;
+  }
+
+  if (error = FT_Get_Glyph(face->glyph, &glyph)) {
+    printf("FT_Get_Glyph error: 0x%x\n", error);
+    return 1;
+  }
+
+  FT_Glyph_Get_CBox(glyph, ft_glyph_bbox_subpixels, &bbox);
+
+  printf("CBox [%s]: xMin: %ld yMin: %ld xMax: %ld yMax: %ld\n",
+         name, bbox.xMin, bbox.yMin, bbox.xMax, bbox.yMax);
+
+  return 0;
+}
+
+
+int
+main(int argc, char** argv)
 {
   int error;
   FT_Library library;
   FT_Face face;
   FT_Matrix transform = { 65536 / HORIZ_HINTING, 0, 0, 65536 };
   FT_ULong charcode;
-  FT_Glyph glyph;
-  FT_BBox bbox;
 
   sscanf(argv[2], "%lu", &charcode);
 
@@ -54,20 +79,17 @@ int main(int argc, char** argv)
   }
  #endif
 
-  if (error = FT_Load_Char(face, charcode, FT_LOAD_NO_HINTING)) {
-    printf("FT_Load_Char error: 0x%x\n", error);
+  if (display_cbox("DEFAULT", FT_LOAD_DEFAULT, charcode, face)) {
     return 1;
   }
 
-  if (error = FT_Get_Glyph(face->glyph, &glyph)) {
-    printf("FT_Get_Glyph error: 0x%x\n", error);
+  if (display_cbox("NO_HINTING", FT_LOAD_NO_HINTING, charcode, face)) {
     return 1;
   }
 
-  FT_Glyph_Get_CBox(glyph, ft_glyph_bbox_subpixels, &bbox);
-
-  printf("CBox: xMin: %ld yMin: %ld xMax: %ld yMax: %ld\n",
-         bbox.xMin, bbox.yMin, bbox.xMax, bbox.yMax);
+  if (display_cbox("FORCE_AUTOHINT", FT_LOAD_FORCE_AUTOHINT, charcode, face)) {
+    return 1;
+  }
 
   return 0;
 }
