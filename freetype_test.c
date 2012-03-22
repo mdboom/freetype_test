615024a048be5e44dcbe76b78e0e41ae07b41d72
Michael Droettboom <mdboom@gmail.com>
2012-03-22 11:04:19 -0400

    Also output monochrome render.

--- freetype_test.c
+++ freetype_test.c
@@ -14,7 +14,7 @@ main(int argc, char** argv)
   FT_Glyph glyph;
   FT_BitmapGlyph bitmap_glyph;
   FT_Bitmap bitmap;
-  unsigned long i, j;
+  unsigned long i, j, p;
 
   char *fontfn;
   unsigned long charcode;
@@ -22,6 +22,8 @@ main(int argc, char** argv)
   unsigned long dpi;
   unsigned long stretch;
   char *outputfn;
+  char outputfn_grey[256];
+  char outputfn_mono[256];
   FILE *output;
 
   fontfn = argv[1];
@@ -73,6 +75,8 @@ main(int argc, char** argv)
     }
   }
 
+  /* Output greyscale */
+
   if (error = FT_Load_Char(face, charcode, FT_LOAD_NO_HINTING)) {
     printf("FT_Load_Char error: 0x%x\n", error);
     return 1;
@@ -91,16 +95,51 @@ main(int argc, char** argv)
   bitmap_glyph = (FT_BitmapGlyph)glyph;
   bitmap = bitmap_glyph->bitmap;
 
-  output = fopen(outputfn, "w");
+  strcpy(outputfn_grey, outputfn);
+  strcat(outputfn_grey, "_grey.pgm");
 
-  printf("bitmap numgrays %d\n", bitmap.num_grays);
-  printf("bitmap pixel_mode %d\n", bitmap.pixel_mode);
-
+  output = fopen(outputfn_grey, "w");
   fprintf(output, "P2\n%d %d\n256\n\n", bitmap.width, bitmap.rows);
 
   for (i = 0; i < bitmap.rows; ++i) {
     for (j = 0; j < bitmap.width; ++j) {
       fprintf(output, "%03hu ", 255 - bitmap.buffer[i * bitmap.pitch + j]);
+    }
+    fprintf(output, "\n");
+  }
+
+  fclose(output);
+
+  /* Output monochrome */
+
+  if (error = FT_Load_Char(face, charcode, FT_LOAD_NO_HINTING)) {
+    printf("FT_Load_Char error: 0x%x\n", error);
+    return 1;
+  }
+
+  if (error = FT_Get_Glyph(face->glyph, &glyph)) {
+    printf("FT_Get_Glyph error: 0x%x\n", error);
+    return 1;
+  }
+
+  if (error = FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_MONO, 0, 1)) {
+    printf("FT_Glyph_To_Bitmap error: 0x%x\n", error);
+    return 1;
+  }
+
+  bitmap_glyph = (FT_BitmapGlyph)glyph;
+  bitmap = bitmap_glyph->bitmap;
+
+  strcpy(outputfn_mono, outputfn);
+  strcat(outputfn_mono, "_mono.pgm");
+
+  output = fopen(outputfn_mono, "w");
+  fprintf(output, "P2\n%d %d\n1\n\n", bitmap.width, bitmap.rows);
+
+  for (i = 0; i < bitmap.rows; ++i) {
+    for (j = 0; j < bitmap.width; ++j) {
+      p = bitmap.buffer[i * bitmap.pitch + (j >> 3)] & (1 << (7 - (j & 0x7)));
+      fprintf(output, "%01hu ", p ? 0 : 1);
     }
     fprintf(output, "\n");
   }
